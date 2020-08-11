8a0e6517eb5fc89b171a366912ce8b707221f501
FuriosoJack <iam@furiosojack.com>
2020-08-11 15:21:28 -0500

    Se hace actualizacion de rutas

--- src/RastreadorProyectosServiceProvider.php
+++ src/RastreadorProyectosServiceProvider.php
@@ -17,6 +17,7 @@ class RastreadorProyectosServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__."/routes/web.php");
+        $this->loadRoutesFrom(__DIR__."/routes/api.php");
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
